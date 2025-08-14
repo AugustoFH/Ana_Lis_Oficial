71ecb7609f094a1a4f10825d1a58af926786bf67
AugustoFH <gutoferrarih@gmail.com>
2025-08-14 19:27:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -143,4 +143,5 @@ def home():
     return "Ana Lis está online!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta via variável de ambiente
+    app.run(host="0.0.0.0", port=port)
